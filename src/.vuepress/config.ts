200eec1503952a5d7031fe734ff83bb57b113232
Salvely <1247230776@qq.com>
2024-04-01 19:47:52 +0800

    finish the skeleton

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,24 @@
 import { defineUserConfig } from "vuepress";
-import theme from "./theme.js";
+import { getDirname, path } from "vuepress/utils";
+import { viteBundler } from "@vuepress/bundler-vite";
+// import { ohmylive2dPlugin } from "vuepress-plugin-oh-my-live2d";
+import {
+  canvasPlugin,
+  CanvasPluginType,
+} from "./plugins/vuepress-plugin-canvas";
+import { live2DAssistPlugin } from "./plugins/vuepress-plugin-live2DAssist";
+import { gradientCoverPlugin } from "./plugins/vuepress-plugin-gradient-cover";
+import theme from "./theme2.js";
+import { popperPlugin } from "./plugins/vuepress-plugin-popper";
+import { PopperShape } from "@moefy-canvas/theme-popper";
+import { hitokotoPlugin } from "./plugins/vuepress-plugin-hitokoto";
+import { shikiPlugin } from "@vuepress/plugin-shiki";
+import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
+import metingPlugin from "vuepress-plugin-meting2";
 
+const __dirname = getDirname(import.meta.url);
 export default defineUserConfig({
   base: "/",
-
   locales: {
     "/": {
       lang: "en-US",
@@ -16,9 +31,109 @@ export default defineUserConfig({
       description: "vuepress-theme-hope 的博客演示",
     },
   },
+  head: [["meta", { name: "referrer", content: "no-referrer-when-downgrade" }]],
+  alias: {
+    "@MyLink": path.resolve(__dirname, "./components/Mylink.vue"),
+    "@MyCoverLink": path.resolve(__dirname, "./components/MyCoverLink.vue"),
+    "@Design": path.resolve(__dirname, "./data/design.ts"),
+    "@Api": path.resolve(__dirname, "./data/api.ts"),
+    "@theme-hope/modules/blog/components/BlogHero": path.resolve(
+      __dirname,
+      "./components/BlogHero.vue",
+    ),
+  },
 
-  theme,
+  theme: theme,
 
+  port: 8080,
+
+  bundler: viteBundler({
+    viteOptions: {
+      server: {
+        proxy: {
+          "/bing": {
+            target: "https://cn.bing.com",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/bing/, ""),
+          },
+        },
+      },
+    },
+    // vuePluginOptions: {},
+  }),
+  plugins: [
+    metingPlugin({
+      metingOptions: {
+        global: true,
+        server: "tencent",
+        api: "https://api.injahow.cn/meting/?server=:server&type=:type&id=:id&auth=:auth&r=:r",
+        type: "playlist",
+        mid: "851947617",
+      },
+    }),
+    // 代码高亮
+    shikiPlugin({
+      theme: "one-dark-pro",
+    }),
+    // 一言插件
+    hitokotoPlugin({}),
+    // 鼠标特效插件
+    popperPlugin({
+      config: {
+        shape: PopperShape.Star,
+        size: 1.95,
+        numParticles: 8,
+      },
+    }),
+    // 看板娘辅助插件
+    live2DAssistPlugin({
+      subPageHidden: true,
+    }),
+    // 背景插件
+    canvasPlugin({
+      type: CanvasPluginType.Figure,
+      ribbonOption: {
+        zIndex: 1,
+        alpha: 0.8,
+        size: 90,
+      },
+    }),
+    // 遮罩插件
+    gradientCoverPlugin({}),
+    // 谷歌统计
+    googleAnalyticsPlugin({
+      // 配置项
+      id: "G-R1WPVQFH8L",
+      debug: true,
+    }),
+    // 看板娘插件
+    // ohmylive2dPlugin({
+    //   // 在这里进行配置
+    //   source: "https://cdn.jsdelivr.net/gh/oragekk/blog-assets/live2D",
+    //   models: [
+    //     {
+    //       scale: 0.44,
+    //       path: "/sipeibojue_5/sipeibojue_5.model3.json",
+    //     },
+    //     {
+    //       scale: 0.4,
+    //       path: "/lafei_4/lafei_4.model3.json",
+    //     },
+    //     {
+    //       scale: 1.0,
+    //       path: "/z46_2/z46_2.model3.json",
+    //     },
+    //   ],
+    //   tips: {
+    //     style: {
+    //       width: 150,
+    //       height: 100,
+    //       offsetX: 0,
+    //       offsetY: 90,
+    //     },
+    //   },
+    // }),
+  ],
   // Enable it with pwa
-  // shouldPrefetch: false,
+  shouldPrefetch: false,
 });
--- src/README.md
+++ src/README.md
@@ -2,43 +2,49 @@
 home: true
 layout: BlogHome
 icon: home
-title: Blog Home
-heroImage: https://theme-hope-assets.vuejs.press/logo.svg
-heroText: The name of your blog
-tagline: You can put your slogan here
+title: Home Page
+heroImage: /logo.svg
+heroText: Salvely's Blog
+bgImage: "/assets/home_bg3.jpg"
 heroFullScreen: true
+tagline: Goals determine what you are going to be. —— Julius Erving
+description: 「一生短暂，过程很美」 致力于分享自己涉及和了解的内容，包括但不限于编程语言、AI体验、世界探索者、极客知识| Exploration & geek enthusiast，Full-stack Front-end Engineer，UX Designer | 与你一起发现更大的世界。
+head:
+  - - meta
+    - name: keywords
+      content: oragekk, 上冬十二, Oragekk Blog, 博客, 个人博客, 极客, geek, 大前端, 设计, iOS, Flutter, Vue
 projects:
-  - icon: project
-    name: project name
-    desc: project detailed description
-    link: https://your.project.link
+  - icon: /logo.svg
+    name: 关于本站
+    desc: 这里有关于站点的详细介绍
+    link: /about
 
-  - icon: link
-    name: link name
-    desc: link detailed description
-    link: https://link.address
+  - icon: /assets/link.svg
+    name: 友情链接
+    desc: (●´∀｀●)ﾉ
+    link: /friend
 
-  - icon: book
-    name: book name
-    desc: Detailed description of the book
-    link: https://link.to.your.book
+  - icon: /assets/lyb.svg
+    name: 留言板
+    desc: 欢迎来留言哦~
+    link: /visitorsbook
 
-  - icon: article
-    name: article name
-    desc: Detailed description of the article
-    link: https://link.to.your.article
+  - icon: /assets/shoucang.svg
+    name: 收藏
+    desc: 有趣的内容
+    link: /collect
 
-  - icon: friend
-    name: friend name
-    desc: Detailed description of friend
-    link: https://link.to.your.friend
+  # - icon: friend
+  #   name: 伙伴名称
+  #   desc: 伙伴详细介绍
+  #   link: https://你的伙伴链接
 
-  - icon: https://theme-hope-assets.vuejs.press/logo.svg
-    name: custom item
-    desc: Detailed description of this custom item
-    link: https://link.to.your.friend
+  # - icon: /logo.svg
+  #   name: 自定义项目
+  #   desc: 自定义详细介绍
+  #   link: https://你的自定义链接
 
-footer: customize your footer text
+footer: <div><span>Powered by <a href="https://v2.vuepress.vuejs.org/zh/" target="_blank">VuePress</a></span>|<span>Theme by <a href="https://theme-hope.vuejs.press/zh/" target="_blank">Hope</a></span></div>
 ---
 
 This is a blog home page demo.
